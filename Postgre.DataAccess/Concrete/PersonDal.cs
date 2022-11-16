c1ba2a4722dfc75c7e1991cbec2fababbeb8b440
Turan Ediz Sacakli <turanedizsacakli@yahoo.com.tr>
2022-11-16 23:44:47 +0300

    addedPackages...

---
+++ Postgre.DataAccess/Concrete/PersonDal.cs
@@ -0,0 +1,21 @@
+﻿using Postgre.DataAccess.Abstract;
+using Postgre.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postgre.DataAccess.Concrete
+{
+    public class PersonDal : IPersonDal
+    {
+        //public List<Person> GetAll()
+        //{
+        //    using (resource)
+        //    {
+
+        //    }
+        //}
+    }
+}
---
+++ Postgre.Entities/Concrete/Person.cs
@@ -0,0 +1,23 @@
+﻿using Postgre.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postgre.Entities.Concrete
+{
+    public class Person :IEntity
+    {
+        //[Key]
+        public int PersonId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string FatherName { get; set; }
+        public string MotherName { get; set; }
+        public string Nationality { get; set; }
+        public string Birthday { get; set; }
+        public string BirthCountry { get; set; }
+        public string LocalId { get; set; }
+    }
+}
