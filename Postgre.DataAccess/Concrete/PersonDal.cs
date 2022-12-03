650372b217f2f9a9ba84c84e4ac3d51aa64c28fd
Turan Ediz Sacakli <turanedizsacakli@yahoo.com.tr>
2022-12-03 13:53:25 +0300

    update...

--- Postgre.DataAccess/Concrete/PersonDal.cs
+++ Postgre.DataAccess/Concrete/PersonDal.cs
@@ -9,53 +9,53 @@ using System.Threading.Tasks;
 
 namespace Postgre.DataAccess.Concrete
 {
-    public class PersonDal : IPersonDal
+    public class PersonDal : PostgreEntityRepositoryBase< Person, PostgreContext> , IPersonDal
     {
-        public void Add(Person person)
-        {
-            using (PostgreContext context = new PostgreContext())
-            {
-                context.People.Add(person);
-                context.SaveChanges();
+        //public void Add(Person person)
+        //{
+        //    using (PostgreContext context = new PostgreContext())
+        //    {
+        //        context.People.Add(person);
+        //        context.SaveChanges();
 
-            }
-        }
+        //    }
+        //}
 
-        public void Update(Person person)
-        {
-            //using (PostgreContext context = new PostgreContext())
-            //{
-            //    List<Person> people= context.People.ToList();
-            //    foreach (var person in people)
-            //    {
-            //        person.
-            //    }
-            //    //return context.People.
-            //}
-        }
-        public void Delete(Person person)
-        {
-            using (PostgreContext context = new PostgreContext())
-            {
-               context.People.Remove(person);
-               context.SaveChanges();
-            }
-        }
+        //public void Update(Person person)
+        //{
+        //    //using (PostgreContext context = new PostgreContext())
+        //    //{
+        //    //    List<Person> people= context.People.ToList();
+        //    //    foreach (var person in people)
+        //    //    {
+        //    //        person.
+        //    //    }
+        //    //    //return context.People.
+        //    //}
+        //}
+        //public void Delete(Person person)
+        //{
+        //    using (PostgreContext context = new PostgreContext())
+        //    {
+        //       context.People.Remove(person);
+        //       context.SaveChanges();
+        //    }
+        //}
 
-        public Person Get(Expression<Func<Person, bool>> filter)
-        {
-            using (PostgreContext context = new PostgreContext())
-            {
-                return context.People.SingleOrDefault(filter);
-            }
-        }
+        //public Person Get(Expression<Func<Person, bool>> filter)
+        //{
+        //    using (PostgreContext context = new PostgreContext())
+        //    {
+        //        return context.People.SingleOrDefault(filter);
+        //    }
+        //}
 
-        public List<Person> GetAll(Expression<Func<Person, bool>> filter = null)
-        {
-            using (PostgreContext context = new PostgreContext())
-            {
-                return context.People.ToList();
-            }
-        }
+        //public List<Person> GetAll(Expression<Func<Person, bool>> filter = null)
+        //{
+        //    using (PostgreContext context = new PostgreContext())
+        //    {
+        //        return context.People.ToList();
+        //    }
+        //}
     }
 }
