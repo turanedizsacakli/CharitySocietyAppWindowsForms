2dc65fd054cdac892382f82f643ad7c57be145b6
Turan Ediz Sacakli <turanedizsacakli@yahoo.com.tr>
2023-01-10 22:04:39 +0300

    newUpdate...
    this update is about simple code...

---
+++ Postgre.Entities/Concrete/City.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postgre.Entities.Concrete
+{
+    public class City:IEntity
+    {
+        public int CityId { get; set; }
+        public string CityName { get; set; }
+    }
+}
---
+++ Postgre.Entities/Concrete/Country.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postgre.Entities.Concrete
+{
+    public class Country
+    {
+        public int CountryId { get; set; }
+        public string CountryName { get; set; }
+    }
+}
