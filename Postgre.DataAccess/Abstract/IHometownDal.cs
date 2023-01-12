c8a061ee284b58e551ddc2930dd670b7987e5033
Turan Ediz Sacakli <turanedizsacakli@yahoo.com.tr>
2023-01-12 23:28:21 +0300

    updateDataAccess...
    district/hometown/marital/dataacceess...

---
+++ Postgre.DataAccess/Abstract/IHometownDal.cs
@@ -0,0 +1,13 @@
+﻿using Postgre.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postgre.DataAccess.Abstract
+{
+    public interface IHometownDal:IEntityRepository<Hometown>
+    {
+    }
+}
---
+++ Postgre.DataAccess/Concrete/HometownDal.cs
@@ -0,0 +1,14 @@
+﻿using Postgre.DataAccess.Abstract;
+using Postgre.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postgre.DataAccess.Concrete
+{
+    public class HometownDal: PostgreEntityRepositoryBase<Hometown,PostgreContext>,IHometownDal
+    {
+    }
+}
